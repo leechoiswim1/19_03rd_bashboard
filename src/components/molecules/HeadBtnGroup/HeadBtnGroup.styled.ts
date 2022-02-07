ef204684eeb1d7e15e55a6db95c6e10adf2d537a
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-08 04:05:19 +0900

    Add 헤드버튼그룹 기본 스타일 셋팅 및 index 적용

---
+++ src/components/molecules/HeadBtnGroup/HeadBtnGroup.styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/macro';
+
+export const Wrapper = styled.section`
+  width: 210px;
+  height: 20px;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  line-height: 20px;
+`;
+
+export const Divider = styled.section`
+  width: 2px;
+  background: white;
+  height: 16px;
+  margin: auto;
+`;
