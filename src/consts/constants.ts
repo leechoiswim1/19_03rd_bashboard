9140f811e61adc57fc2c2d7556fec34c0a0db15c
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-09 04:16:56 +0900

    Add 상수 추가 (filterCategory,ProcessMethod,materialList)

--- src/consts/constants.ts
+++ src/consts/constants.ts
@@ -1,3 +1,6 @@
 export const companyName = 'CAPA 파트너스';
 export const navigatorBtn = ['A 가공업체', '로그아웃'];
 export const sideBarBtn = ['파트너정밀가공', '로그아웃'];
+export const filterCategory = ['가공방식', '재료'];
+export const processMethod = ['밀링', '선반'];
+export const materialList = ['알루미늄', '탄소강', '구리', '합금강', '강철'];
