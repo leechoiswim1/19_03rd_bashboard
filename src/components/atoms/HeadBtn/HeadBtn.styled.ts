51e04b4240512bf751ccc8ec4975fb509f19248c
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-08 11:34:14 +0900

    Modify svg 스타일드파일로 이동후  마진조정

--- src/components/atoms/HeadBtn/HeadBtn.styled.ts
+++ src/components/atoms/HeadBtn/HeadBtn.styled.ts
@@ -1,8 +1,16 @@
 import styled from 'styled-components/macro';
+import { ReactComponent as Office } from 'assets/images/office.svg';
 
 export const Wrapper = styled.section`
   height: 20px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
 `;
 export const Text = styled.span`
   font-size: 14px;
+`;
+
+export const OfficeIcon = styled(Office)`
+  margin-right: 8px;
 `;
--- src/components/atoms/HeadBtn/HeadBtn.tsx
+++ src/components/atoms/HeadBtn/HeadBtn.tsx
@@ -1,11 +1,11 @@
 import React, { ReactElement } from 'react';
-import { ReactComponent as Office } from 'assets/images/office.svg';
+
 import * as S from './HeadBtn.styled';
 import HeadBtnProps from './HeadBtn.type';
 
 const HeadBtn = ({ title }: HeadBtnProps): ReactElement => (
   <S.Wrapper>
-    {title === 'A 가공업체' ? <Office /> : ''}
+    {title === 'A 가공업체' ? <S.OfficeIcon /> : ''}
     <S.Text>{title}</S.Text>
   </S.Wrapper>
 );
