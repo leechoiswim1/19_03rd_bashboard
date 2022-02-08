55824e273603ed56fbf02ac4675c682357ec80be
yeongjong <tohero21@naver.com>
2022-02-08 17:22:46 +0900

    Add - a11y Hidden 접근성 숨기기 css 추가

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 import reset from 'styled-reset';
 
 export const GlobalStyledComponent = createGlobalStyle`
@@ -6,4 +6,17 @@ export const GlobalStyledComponent = createGlobalStyle`
     ${reset}
     box-sizing: border-box;
   }
+`;
+
+export const a11yHidden = css`
+  overflow: hidden;
+  position: absolute;
+  clip: rect(0, 0, 0, 0);
+  clip-path: circle(0);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  white-space: nowrap;
 `;
