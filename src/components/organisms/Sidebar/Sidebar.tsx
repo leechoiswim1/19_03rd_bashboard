140ab5debe5e731f03ecc4dbadda995a49d62e6c
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-08 10:35:08 +0900

    Add Sidebar 컴포넌트 추가

---
+++ src/components/organisms/Sidebar/Sidebar.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react';
+
+import * as S from './Sidebar.styled';
+
+const Sidebar: React.FC = (): ReactElement => <S.Wrapper>어쩌고</S.Wrapper>;
+
+export default Sidebar;
