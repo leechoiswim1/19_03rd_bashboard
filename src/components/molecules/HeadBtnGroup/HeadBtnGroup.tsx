923df6ec3d3bf51f02cfad1c9607eb70de888188
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-08 14:18:52 +0900

    Modify 컴포넌트 import 방식 수정

--- src/components/molecules/HeadBtnGroup/HeadBtnGroup.tsx
+++ src/components/molecules/HeadBtnGroup/HeadBtnGroup.tsx
@@ -1,10 +1,10 @@
 import React, { ReactElement } from 'react';
-import { HeadBtn } from 'components';
+import { HeadBtn } from 'components/atoms';
 import * as S from './HeadBtnGroup.styled';
 
 const HeadBtnGroup = (): ReactElement => (
   <S.Wrapper>
-    <HeadBtn title="A 가공업체" />
+    <HeadBtn title="가공업체" />
     <S.Divider />
     <HeadBtn title="로그아웃" />
   </S.Wrapper>
