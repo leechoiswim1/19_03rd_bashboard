1089bf8fdccdec1bf5e5ae30e6686ca423b1a7a5
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-08 10:42:29 +0900

    Add 타이틀 분류에 따른 svg 연결

--- src/components/atoms/HeadBtn/HeadBtn.tsx
+++ src/components/atoms/HeadBtn/HeadBtn.tsx
@@ -1,10 +1,11 @@
 import React, { ReactElement } from 'react';
-import { SCProps } from 'types/props';
+import { ReactComponent as Office } from 'styles/images/office.svg';
 import * as S from './HeadBtn.styled';
 import HeadBtnProps from './HeadBtn.type';
 
-const HeadBtn = ({ title }: SCProps<HeadBtnProps>): ReactElement => (
+const HeadBtn = ({ title }: HeadBtnProps): ReactElement => (
   <S.Wrapper>
+    {title === 'A 가공업체' ? <Office /> : ''}
     <S.Text>{title}</S.Text>
   </S.Wrapper>
 );
