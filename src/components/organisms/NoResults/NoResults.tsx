c763f1ea513ec82097d35530f697e3e2a6bf7809
hyun <vi2920va@gmail.com>
2022-02-09 04:06:46 +0900

    Add - NoResult 추가

---
+++ src/components/organisms/NoResults/NoResults.tsx
@@ -0,0 +1,6 @@
+import React, { ReactElement } from 'react';
+import * as S from './NoResults.styled';
+
+const NoResults = (): ReactElement => <S.Wrapper>조건에 맞는 견적 요청이 없습니다</S.Wrapper>;
+
+export default NoResults;
