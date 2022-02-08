f3f796b167eba621ed783fdb38c5eea40428b480
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-08 14:14:00 +0900

    Add 사이드바 스타일링

--- src/components/organisms/Sidebar/Sidebar.styled.ts
+++ src/components/organisms/Sidebar/Sidebar.styled.ts
@@ -1,3 +1,39 @@
 import styled from 'styled-components/macro';
 
-export const Wrapper = styled.div``;
+export const Background = styled.div`
+  position: fixed;
+  overflow-x: hidden;
+  overflow-y: auto;
+  outline: 0;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 1000;
+`;
+export const Overlay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
+
+export const Wrapper = styled.div`
+  width: 280px;
+  height: 100%;
+  position: absolute;
+  background: white;
+  top: 0;
+  left: 0;
+`;
+
+export const Header = styled.div`
+  height: 50px;
+  border-bottom: 2px solid #e5e5e5;
+  box-sizing: border-box;
+  padding: 16px 20px;
+`;
+export const Text = styled.span`
+  color: #2196f3;
+  font-weight: 900;
+`;
--- src/components/organisms/Sidebar/Sidebar.tsx
+++ src/components/organisms/Sidebar/Sidebar.tsx
@@ -1,7 +1,16 @@
 import React, { ReactElement } from 'react';
-
+import { Logo } from 'components/atoms';
 import * as S from './Sidebar.styled';
 
-const Sidebar = (): ReactElement => <S.Wrapper>어쩌고</S.Wrapper>;
+const Sidebar = (): ReactElement => (
+  <S.Background>
+    <S.Overlay />
+    <S.Wrapper>
+      <S.Header>
+        <Logo path="sidebar" />
+      </S.Header>
+    </S.Wrapper>
+  </S.Background>
+);
 
 export default Sidebar;
