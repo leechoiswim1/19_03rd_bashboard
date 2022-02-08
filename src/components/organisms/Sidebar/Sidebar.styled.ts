7dd62ac4a7ad232cf13a134b33febcabb77b90af
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-08 20:50:24 +0900

    Add 사이드바 컴포넌트 스타일 추가

--- src/components/organisms/Sidebar/Sidebar.styled.ts
+++ src/components/organisms/Sidebar/Sidebar.styled.ts
@@ -1,39 +1,44 @@
 import styled from 'styled-components/macro';
+import SidebarProps from './Sidebar.type';
 
-export const Background = styled.div`
-  position: fixed;
-  overflow-x: hidden;
-  overflow-y: auto;
-  outline: 0;
+export const Overlay = styled.div<SidebarProps>`
+  display: ${props => (props.openSideMenu ? 'flex' : 'none')};
+  position: absolute;
   top: 0;
   left: 0;
-  bottom: 0;
-  right: 0;
-  z-index: 1000;
-`;
-export const Overlay = styled.div`
-  position: absolute;
   width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.3);
+  height: 100vh;
+  box-sizing: border-box;
+  background-color: rgba(0, 0, 0, 0.2);
+  @media (min-width: 769px) {
+    display: none;
+  }
 `;
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.div<SidebarProps>`
   width: 280px;
   height: 100%;
   position: absolute;
   background: white;
   top: 0;
   left: 0;
+  visibility: ${props => (props.openSideMenu ? 'visible' : 'hidden')};
+  transform: ${props => (props.openSideMenu ? 'translateX(0px)' : 'translateX(-100%)')};
+  transition: transform 0.5s ease-in-out;
+  @media (min-width: 769px) {
+    display: none;
+  }
 `;
 
 export const Header = styled.div`
-  height: 50px;
+  height: 44px;
   border-bottom: 2px solid #e5e5e5;
   box-sizing: border-box;
   padding: 16px 20px;
+  display: flex;
+  align-items: center;
 `;
-export const Text = styled.span`
-  color: #2196f3;
-  font-weight: 900;
+
+export const Content = styled.div`
+  padding: 36px 32px;
 `;
--- src/components/organisms/Sidebar/Sidebar.tsx
+++ src/components/organisms/Sidebar/Sidebar.tsx
@@ -1,16 +1,21 @@
 import React, { ReactElement } from 'react';
 import { Logo } from 'components/atoms';
+import { HeadBtnGroup } from 'components/molecules';
 import * as S from './Sidebar.styled';
+import SidebarProps from './Sidebar.type';
 
-const Sidebar = (): ReactElement => (
-  <S.Background>
-    <S.Overlay />
-    <S.Wrapper>
+const Sidebar = ({ openSideMenu, handleSideMenu }: SidebarProps): ReactElement => (
+  <>
+    <S.Overlay openSideMenu={openSideMenu} onClick={handleSideMenu} />
+    <S.Wrapper openSideMenu={openSideMenu}>
       <S.Header>
         <Logo path="sidebar" />
       </S.Header>
+      <S.Content>
+        <HeadBtnGroup path="sidebar" />
+      </S.Content>
     </S.Wrapper>
-  </S.Background>
+  </>
 );
 
 export default Sidebar;
