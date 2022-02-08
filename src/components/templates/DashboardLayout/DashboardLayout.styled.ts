0bbfe55614b54efe473870116a3068e2aa426153
yeongjong <tohero21@naver.com>
2022-02-08 17:23:19 +0900

    Add - 들어온 요청란 반응형 구현 완료(데스크탑, 테블릿, 모바일)

--- src/components/templates/DashboardLayout/DashboardLayout.styled.ts
+++ src/components/templates/DashboardLayout/DashboardLayout.styled.ts
@@ -1,9 +1,19 @@
 import styled from 'styled-components/macro';
 
 export const Layout = styled.div`
-  margin: 24px 20px 20px;
   display: flex;
+  max-width: 1130px;
   flex-direction: column;
+  margin: 24px 20px 20px;
+
+  ${props => props.theme.breakpoints.tablet} {
+    margin: 40px 75px 60px;
+  }
+
+  ${props => props.theme.breakpoints.desktop} {
+    margin: 40px auto 60px;
+    padding: 0 75px;
+  }
 `;
 
 export const DummyCard = styled.article`
@@ -12,8 +22,22 @@ export const DummyCard = styled.article`
   background-color: #444;
 `;
 
+export const HiddenDummyCard = styled(DummyCard)`
+  opacity: 0;
+  visibility: hidden;
+`;
+
 export const RequestCardWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
+
+  ${props => props.theme.breakpoints.tablet} {
+    flex-direction: row;
+    flex-wrap: wrap;
+    > * {
+      flex-basis: 366px;
+      flex-grow: 1;
+    }
+  }
 `;
