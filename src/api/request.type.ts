30998acc71082a7af3e98de2dbf8252ef0c15518
hyun <vi2920va@gmail.com>
2022-02-08 14:31:43 +0900

    Add - axios get 테스트

---
+++ src/api/request.type.ts
@@ -0,0 +1,15 @@
+type Status = '대기중' | '상담중';
+type Method = '밀링' | '선반';
+type Material = '알루미늄' | '탄소강' | '구리' | '스테인리스강' | '강철';
+
+export interface Request {
+  id: number;
+  title: string;
+  client: string;
+  due?: string;
+  count?: number;
+  amount: number;
+  method: Method[];
+  material: Material[];
+  status: Status;
+}
