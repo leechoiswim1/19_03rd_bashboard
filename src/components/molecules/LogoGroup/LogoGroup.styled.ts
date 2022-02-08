eaa3718d2121aa7371ffbaaa96f00448bdc676fb
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-08 20:53:13 +0900

    Add 햄버거 버튼 컴포넌트 추가

--- src/components/molecules/LogoGroup/LogoGroup.styled.ts
+++ src/components/molecules/LogoGroup/LogoGroup.styled.ts
@@ -1,16 +1,25 @@
 import styled from 'styled-components/macro';
 import { ReactComponent as Hambuger } from 'assets/images/hamburger.svg';
+import LogoGroupProps from './LogoGroup.type';
 
 export const Wrapper = styled.div`
-  @media (max-width: 768px) {
-    display: flex;
-  }
+  display: flex;
 `;
 
-export const HambugerBtn = styled(Hambuger)`
-  display: none;
+export const HamburgerWrap = styled.button`
+  background: none;
+  border: none;
+  padding: 0px;
+  cursor: pointer;
   @media (max-width: 768px) {
     display: block;
     margin: auto 19px auto 0px;
+  }
+`;
+
+export const HambugerBtn = styled(Hambuger)<LogoGroupProps>`
+  display: none;
+  @media (max-width: 768px) {
+    display: block;
   }
 `;
