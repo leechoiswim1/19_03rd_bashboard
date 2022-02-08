093ab02f136d9b4fabcadbcc68274369333876cb
b41-41 <41@b41.kr>
2022-02-08 20:19:16 +0900

    Style - Card 스타일링

--- src/components/molecules/Card/Card.styled.ts
+++ src/components/molecules/Card/Card.styled.ts
@@ -1,12 +1,46 @@
 import styled from 'styled-components/macro';
 
-export const Card = styled.div`
+export const CardWrapper = styled.div`
     width: 100%;
   padding: 24px 16px;
   margin: 0 8px;
   border-radius: 4px;
-  border: 1px solid
+  border: 1px solid ${({ theme }) => theme.color.borderGray}
     &:hover{
         border: 2px solid ${({ theme }) => theme.color.primaryBlue}
     }
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Title = styled.h3`
+  color: ${({ theme }) => theme.color.black};
+  font-size: ${({ theme }) => theme.fontSize.m};
+  line-height: ${({ theme }) => theme.lineHeight.m};
+`;
+
+export const Client = styled.h4`
+  margin-bottom: 24px;
+  font-size: ${({ theme }) => theme.fontSize.s};
+  line-height: ${({ theme }) => theme.lineHeight.s};
+`;
+
+export const Due = styled.div`
+  color: ${({ theme }) => theme.color.gray};
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+`;
+
+export const ContentTitle = styled.div`
+  color: ${({ theme }) => theme.color.gray};
+`;
+
+export const Contents = styled.div`
+  color: ${({ theme }) => theme.color.gray};
+  font-weight: 700;
 `;
